a335d0890ae3afb1cb643656088288db2046ed66
root <root@adminuser-VirtualBox>
2022-10-26 16:02:04 +0200

    20221026 1601

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -20,7 +20,7 @@ const config = {
   // If you aren't using GitHub pages, you don't need these.
   organizationName: 'drogovic', // Usually your GitHub org/user name.
   projectName: 'docusaurus-useitcloud', // Usually your repo name.
-
+  plugins: [require.resolve("@cmfcmf/docusaurus-search-local")],
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
   // to replace "en" with "zh-Hans".
